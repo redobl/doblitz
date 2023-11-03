4216b2b9852dbf23af5c690e92823068d85b21a6
head47 <63517545+head47@users.noreply.github.com>
2023-11-04 01:30:08 +0300

    change character format

--- common/models.py
+++ common/models.py
@@ -11,11 +11,29 @@ class BaseModel(peewee.Model):
 class Character(BaseModel):
     name = peewee.CharField()
     player_id = peewee.IntegerField(null=True, index=True)
+    active = peewee.BooleanField(default=True)
+
     hp = peewee.IntegerField(default=100, null=True)
     max_hp = peewee.IntegerField(default=100, null=True)
-    active = peewee.BooleanField(default=True)
+    ap = peewee.IntegerField(default=100, null=True)
+    max_ap = peewee.IntegerField(default=100, null=True)
+
     coordX = peewee.IntegerField(null=True)
     coordY = peewee.IntegerField(null=True)
+    layer = peewee.IntegerField(default=0, null=True)
+
+    sizeX = peewee.IntegerField(default=4)
+    sizeY = peewee.IntegerField(default=4)
+    height = peewee.IntegerField(default=2)
+
+    inventory = peewee.CharField(default="{}")
+    inventorySize = peewee.IntegerField(default=10)
+
+    abilities = peewee.CharField(default="{}")
+    maxAbilities = peewee.IntegerField(default=10)
+
+    effects = peewee.CharField(default="{}")
+    extra = peewee.CharField(default="{}")
 
 
 def init_db(path: str) -> peewee.Database:
