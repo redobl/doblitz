0622eb6a3e752a49b381ff8211bc1d46ac66eb51
CreativeeBlackWolf <32525436+CreativeeBlackWolf@users.noreply.github.com>
2023-11-20 21:04:57 +0300

    WIP: add_object_popup

--- GUI/utils/add_object_popup.py
+++ GUI/utils/add_object_popup.py
@@ -1,11 +1,24 @@
+from kivy.properties import BooleanProperty, NumericProperty
 from kivy.uix.popup import Popup
+from tiled_map_manager import TileMap
 
 
 class AddObjectPopup(Popup):
+    x_coords = NumericProperty()
+    y_coords = NumericProperty()
+    is_absolute_coords = BooleanProperty()
+
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
 
+        self.tiled_map: TileMap = None
+
+    def open(self, tiled_map: TileMap, *_args, **kwargs):
+        self.tiled_map = tiled_map
+        return super().open(*_args, **kwargs)
+
     def add_object(self):
-        print("NOT IMPLEMENTED YET")
+        if not self.tiled_map:
+            print("how.")
 
         self.dismiss()
