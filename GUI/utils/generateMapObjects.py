60fb57d583b7d447fdc854e707dbed3f57180c77
CreativeeBlackWolf <32525436+CreativeeBlackWolf@users.noreply.github.com>
2024-01-04 03:45:55 +0300

    update object generator to randomize layer and height

--- GUI/utils/generateMapObjects.py
+++ GUI/utils/generateMapObjects.py
@@ -9,6 +9,7 @@ main_dir = Path(os.path.dirname(__file__)) / ".." / ".."
 sys.path.insert(1, str(main_dir))
 from common.game import MapObject
 from common.models import init_db
+from GUI.utils.progressBar import progressBar
 
 MAP_SIZE = (3840, 1920)
 
@@ -17,10 +18,15 @@ def main():
     db_path = main_dir / "db.sqlite"
     init_db(db_path)
     MapObject.clear()
-    for i in range(10000):
+    for i in progressBar(range(10000), "Generating: "):
         coords = (random.randint(0, MAP_SIZE[0]), random.randint(0, MAP_SIZE[1]))
-        MapObject.create(name=f"Object {i}", location_x=coords[0], location_y=coords[1])
-        print(f"generated {i}")
+        MapObject.create(
+            name=f"Object {i}", 
+            location_x=coords[0], 
+            location_y=coords[1],
+            layer=random.randint(0, 128),
+            height=random.randint(0, 2)
+        )
 
 
 if __name__ == "__main__":
